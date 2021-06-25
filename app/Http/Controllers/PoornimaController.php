3959d8cc8b668cd9bda933bab4e234a1d36169df
Your Name <poornimamadappally@gmail.com>
2021-06-25 13:42:31 +0530

    dummy payment

---
+++ app/Http/Controllers/PoornimaController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Support\Facades\Request;
+use Razorpay\Api\Api;
+
+class PoornimaController extends Controller
+{
+    protected function subscribe(){
+        $request=Request::all();
+        $api = new Api(env('RAZORPAY_KEY'), env('RAZORPAY_SECRET'));
+        $subscription  = $api->subscription->create(array(
+                'plan_id' =>$request['plan'],
+                'customer_notify' => 0,
+                'total_count' => 6,
+            )
+        );
+        return $subscription;
+    }
+}
